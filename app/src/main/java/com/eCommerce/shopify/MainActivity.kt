2dd9ed410aff89311549a9dc99f625d213c02f98
Esraa Sayed <esraasayedahmed1999@gmail.com>
2022-05-23 20:08:08 +0200

    first commit

---
+++ app/src/main/java/com/eCommerce/shopify/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eCommerce.shopify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
