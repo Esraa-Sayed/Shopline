d17b224dc847fb459d342579014f75136fdb0bce
Thoria hamdy <Thoriahamdy99@gmail.com>
2022-05-26 04:05:32 +0200

    adding shopping cart screen ui, no custom action bar, i will add it later

---
+++ app/src/main/java/com/eCommerce/shopify/ui/shopping_cart/view_model/ShoppingCartViewModel.kt
@@ -0,0 +1,7 @@
+package com.eCommerce.shopify.ui.shopping_cart.view_model
+
+import androidx.lifecycle.ViewModel
+
+class ShoppingCartViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
